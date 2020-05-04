d17a7421bd5a26748f4934cadbda67888a82a72c
annagitel <asandler@redhat.com>
2020-05-04 18:52:49 +0300

    main changes

--- main.py
+++ main.py
@@ -29,18 +29,37 @@ def get_current_services():
     process_list = {}
     for proc in psutil.process_iter():
         try:
-            processName = str(proc.name())
-            processID = proc.pid()
-            process_list.update({processName, processID})
+            processName = str(proc.name)
+            processID = proc.pid
+            process_list.update({processName: processID})
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
     return process_list
 
 
+def write_to_serviceList(ts_obj):
+    pass
+
+
+def write_to_statusLog(diff_list):
+    pass
+
+
 def monitor(time_diff):
-    current_time = time.clock()
+    crnt_ts = TIME_STAMP
+    prev_ts = crnt_ts
+    while True:
+        current_time = time.clock()
+        pro_list = get_current_services()
+        crnt_ts = TIME_STAMP(time=current_time, pro_list=pro_list)
+        write_to_serviceList(current_time)
+        diffs = diff(crnt_ts, prev_ts)
+        write_to_statusLog(diffs)
+        prev_ts = crnt_ts
+
+"""
     current_process_list = get_current_services()
-    current_time_stamp = time_stamp.get_from_dict({current_time, current_process_list})
+    current_time_stamp = time_stamp.get_from_dict({current_time,current_process_list})
     previouse_time_stamp = current_time_stamp
 
     while True:
@@ -55,7 +74,7 @@ def monitor(time_diff):
             file.write(json.dumps(diffs))
 
         previouse_time_stamp = current_time_stamp
-        time.sleep(time_diff)
+        time.sleep(time_diff)"""
 
 
 def manual(start_time, end_time):
@@ -74,7 +93,7 @@ def manual(start_time, end_time):
 
 
 if __name__ == '__main__':
-    mode = input("Hello user, please choose a mode \n 2 - monitor \n 1 - manual \n 0 - exit")
+    mode = int(input("Hello user, please choose a mode \n 2 - monitor \n 1 - manual \n 0 - exit"))
     if mode == 0:
         exit()
     elif mode == 1:
